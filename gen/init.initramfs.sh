1a57a9079314b39bd4bbab3510936513303a1b74
Noktoborus <noktoborus@gmail.com>
2011-08-22 11:11:16 +1100

    upadte init script

--- gen/init.initramfs.sh
+++ gen/init.initramfs.sh
@@ -18,13 +18,39 @@ pre_mount ()
 	fi
 }
 
+give_shell ()
+{
+	echo "## start shell"
+	sh
+	return 1
+}
+
+get_if_cfg ()
+{
+	[ -z "$eif" ] && return 1
+	cat /proc/cmdline | sed 's/ /\n/g' | sed 's/.*eif_'$eif'=\([^ ]*\).*\|.*/\1/'
+	return 0
+}
+
+get_ifs ()
+{
+	ip link show | sed -e 's/[0-9]*: \([^ :]*\).*\|.*/\1/' -e '/^$/d'
+}
+
+_is_debug ()
+{
+	echo " `cat /proc/cmdline` " | grep ' kzdebug '
+}
+
 echo "*** mount base"
 touch /etc/mtab
 pre_mount M
 
+_is_debug && give_shell
+
 echo "*** get root path"
 NBD_ROOT=$(cat /proc/cmdline | sed 's/.*kznbd=\([a-zA-Z0-9.]*\).*\|.*/\1/')
-[ -z "$NBD_ROOT" ] && exit 1
+[ -z "$NBD_ROOT" ] && give_shell
 NBD_PORT=$(echo "$NBD_ROOT" | sed 's/.*:\([0-9]*\)\|.*/\1/')
 
 if [ ! -z "$NBD_PORT" ];
@@ -40,19 +66,35 @@ mdev -s
 
 echo "*** modprobe aufs"
 insmod /lib/modules/`uname -r`/misc/aufs.ko
-[ $? -ne 0 ] && exit 1
+[ $? -ne 0 ] && give_shell
 
 echo "*** configure network"
-udhcpc -s /sbin/udhcpc-script.sh -i eth0
-[ $? -ne 0 ] && exit 1
+get_ifs | while read eif;
+do
+	[ -z "$eif" ] && continue
+	echo "** configure $eif"
+	get_if_cfg "$eif" | while read cfg;
+	do
+		[ -z "$cfg" ] && continue
+		echo "**	$cfg"
+		if [ "$cfg" = "dhcp" ];
+		then
+			udhcpc -s /sbin/udhcpc-script.sh -i eth0
+		else
+			ip link set up dev "$eif"
+			ip address add $cfg dev "$eif"
+		fi
+	done
+done
+[ $? -ne 0 ] && give_shell
 
 echo "*** configure nbd"
 nbd-client "$NBD_ROOT" "$NBD_PORT" /dev/nbd0 -p
-[ $? -ne 0 ] && exit 1
+[ $? -ne 0 ] && give_shell
 
 echo "*** mount nbd"
 mount /dev/nbd0 /mnt
-[ $? -ne 0 ] && exit 1
+[ $? -ne 0 ] && give_shell
 
 echo "*** killall"
 killall -9 udhcpc
@@ -64,3 +106,4 @@ echo "*** starting new root"
 exec switch_root /mnt /sbin/init
 
 echo "@@@ FAIL"
+exec sh
